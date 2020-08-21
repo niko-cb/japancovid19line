ef4f8072a2ec5fc33769a58b8c7c80af7fab4bec
Christian Burgos <45473563+niko-cb@users.noreply.github.com>
2020-08-21 20:48:33 +0900

    Updated Source (#13)
    * update readme (#11)
    
    * fixed empty japanese name...hopefully

--- app/controller/logic/scrape/format.go
+++ app/controller/logic/scrape/format.go
@@ -19,16 +19,18 @@ func formatData(bytes []byte) ([]*models.PrefectureData, error) {
 		if p.NameJA == "" {
 			p.NameJA = (&prefectures.PrefectureMap{}).Japanese(p.Name)
 		}
-		cities, err := json.Marshal(p.ConfirmedByCity)
-		if err != nil {
-			return nil, err
+		if p.NameJA != "" {
+			cities, err := json.Marshal(p.ConfirmedByCity)
+			if err != nil {
+				return nil, err
+			}
+
+			prefectureData := models.NewPrefectureData(p.NameJA, p.Confirmed,
+				p.Deaths, p.Recovered, p.NewlyConfirmed,
+				p.YesterdayConfirmed, string(cities))
+
+			pData = append(pData, prefectureData)
 		}
-
-		prefectureData := models.NewPrefectureData(p.NameJA, p.Confirmed,
-			p.Deaths, p.Recovered, p.NewlyConfirmed,
-			p.YesterdayConfirmed, string(cities))
-
-		pData = append(pData, prefectureData)
 	}
 	return pData, nil
 }
